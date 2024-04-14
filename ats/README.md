2a60b363fdd4235ef6ee4ce2ea497f9ad0c11fe5
dtakakuwa129 <daisuke.takakuwa129@gmail.com>
2024-04-14 13:44:37 +0900

    configure webpack

---
+++ ats/README.md
@@ -0,0 +1,96 @@
+# 技術スタック
+
+## =======アプリ関連=======
+
+基盤
+
+- Node.js(14.15.4)
+
+React
+
+- React(16.3.2)
+- react-dom(16.3.2)
+- react-router(3.2.0)
+- react-router-scroll(0.4.2)
+- recompose(0.26.0)
+
+Redux
+
+- redux(3.7.2)
+- redux-async-loader(1.2.3)
+- redux-effects-steps(1.0.1)
+- react-router-redux(4.0.8)
+- react-redux(5.0.7)
+
+## =======ビルド関連=======
+
+Webpack
+
+- webpack(4.6.0)
+- webpack-bundle-analyzer(2.11.1)
+- webpack-cli(3.1.1)
+- webpack-dev-server(3.1.3)
+- webpack-merge(4.1.2)　※storybook 用？
+- html-webpack-plugin(3.1.0)
+
+Babel(基本設定)
+
+- babel-loader(8.0.0-beta.1)
+
+Babel(Presets)
+
+- @babel/preset-env(7.0.0-beta.44)
+- @babel/preset-react(7.0.0-beta.44)
+
+Babel(Plugins)
+
+- babel-plugin-module-resolver(3.1.0)
+- babel-plugin-dual-import(1.2.1)
+- react-hot-loader(4.1.2) ※`webpack-dev-server --hot`で事足りてるから不要
+- @babel/plugin-proposal-object-rest-spread(7.0.0-beta.44)
+- @babel/plugin-syntax-dynamic-import(7.0.0-beta.44)
+
+CSS
+
+- style-loader(0.20.3)
+- css-loader(0.28.11)
+- extract-text-webpack-plugin(4.0.0-beta.0)
+- postcss-loader(2.1.2)
+
+静的ファイル(画像)
+
+- url-loader(1.0.1)
+
+## ======= webpack 関連 =======
+
+- webpack 単体は【JS】ファイルしか扱わない → **JS ファイル以外(CSS,画像) は loader に任せる。**
+- バンドル/ビルド に対して Plugin でカスタマイズする。
+- **バンドル(Loader 実行) → Plugin 実行**
+
+<br>
+
+### **CSS（css loader → style-loader → extract-text-webpack-plugin）**
+
+React application(JS modules)<br>
+　 ↓<br>
+　 ↓ 【**css loader**】 collect CSS refered in Javascript modules<br>
+　 ↓<br>
+String<br>
+　 ↓<br>
+　 ↓ 【**style-loader**】 put String into \<style> tags in index.html<br>
+　 ↓<br>
+index.html with all styles in \<style> tags<br>
+　 ↓<br>
+　 ↓ 【**extract-text-webpack-plugin**】 extract \<style> tags into CSS file<br>
+　 ↓<br>
+index.html with `<link rel>`
+
+全ての style が index.html の\<head>内に定義されるのは読みづらい。<br>
+→ CSS ファイルが別のファイルに切り出すのが`extract-text-webpack-plugin`
+
+<br>
+
+# TODO
+
+1. webpack.config.js の書き方を学ぶ
+2.
---
+++ ats/webpack.config.js
@@ -0,0 +1,100 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = (env, args) => ({
+  // development: デバッグしやすい形でバンドル
+  // production:  パフォーマンス効率のよい形でバンドル
+  mode: args.mode || "development",
+  // バンドルの入口となるJSファイル
+  entry: "./src/index.js",
+  // バンドルのOUTPUT
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    // ビルドごとに異なるハッシュ値を生成する -> キャッシュクリア
+    filename: "[name]-[hash].js",
+    // １つのファイルだけにバンドルすることが不効率な場合に複数のチャンクファイルを生成する。
+    chunkFilename: "[name]-[chunkhash].js",
+    // バンドルされた静的リソース(JSファイル、CSSファイル、画像)のベースパス
+    publicPath: "/",
+  },
+  // webpack-dev-server実行時の設定
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    port: 9000,
+  },
+
+  // module = Loaderの設定
+  // Loader = 「JavaScript以外のリソース（画像、CSS）」を処理する方法を定義する
+  module: {
+    // Loaderを定義していく
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          // ここでoptionで定義してもいいし、.babelcに切り出してもOK
+          //   options: {
+          //     presets: ["@babel/preset-env", "@babel/preset-react"],
+          //   },
+        },
+      },
+      // CSSが <style>タグではなく、cssファイルに出力されるように
+      // https://v4.webpack.js.org/plugins/extract-text-webpack-plugin/#usage
+      {
+        // sass-loader使わずとも、これでCSSに変換されるっぽいわ。
+        test: /\.css|.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader",
+        }),
+      },
+      // 静的ファイル(画像)をインライン化(DataURL化)するかどうかをファイルサイズで決める。
+      // 　limit未満の小さいファイル -> 同じHTMLファイル内にインライン化する。
+      // 　limit以上の大きいファイル -> 画像ファイルに出力する。
+      // 〇インライン化する = HTTPリクエスト数が減る。
+      // ✕インライン化すると、画像だけ差し替えたい場合にHTMLファイルごと更新必要
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192, // ファイルサイズの上限（バイト単位）
+              name: "images/[name].[hash:8].[ext]", // 出力ファイル名のフォーマット
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  // Plugin = webpackのビルドプロセスを拡張・カスタマイズする
+  plugins: [
+    // CSSが <style>タグではなく、cssファイルに出力されるように
+    new ExtractTextPlugin({
+      filename: "app.css",
+      allChunks: true,
+    }),
+    // バンドルで生成したJSファイルは、毎ビルド異なるファイル名となる。
+    // 本プラグインで、自動で <script src="/[name]-[hash].js" /> を index.htmlに記述する。
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      // <link rel="shortcut icon" href="/favicon.png"> <head>内に自動追加する。
+      favicon: "./public/favicon.png",
+      // キャッシュ対策。ファイル名の末尾に?[hash]をつける。ファイル名自体にhashあればこれは不要？
+      hash: true,
+      // バンドルで生成するHTMLファイルをどう最小化するか。
+      minify: {
+        collapseWhitespace: true, // 不要な空白文字を削除
+        removeComments: true,
+        removeRedundantAttributes: true, // 不要な属性を削除 ex: <input type="text" の はデフォルト値なので不要
+        removeStyleLinkTypeAttributes: true, // link, styleタグの不要なDefault属性指定を消す。 removeRedundantAttributesでも消えるはず
+        useShortDoctype: true, // DOCTYPE宣言を小文字に ※あんま効果が分からん
+        keepClosingSlash: true, // HTML5では <img src="example.jpg"> はOKだが、 /> を追加する。
+      },
+    }),
+  ],
+});
